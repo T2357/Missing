27e853588e63fba8ff3e7bd8a6aa68133b8a72dd
Tushar kumar <89009964+T2357@users.noreply.github.com>
2022-02-14 22:13:19 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <div><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php"><img src=images/logo4.jpg></a>
+  <a class="navbar-brand" href="index.php"><img src=images/logo4.JPG></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
